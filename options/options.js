7362b2207ad22e2f4ef8e838136866e60dbab027
Crowdbotics <team@crowdbotics.com>
2024-02-21 17:48:10 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "icy-heart-46541",
+  url: "https://icy-heart-46541.botics.co",
+  api: "https://icy-heart-46541.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
